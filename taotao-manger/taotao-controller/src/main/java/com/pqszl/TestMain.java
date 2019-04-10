2212ff62b31768f9a2ed4a694937f04515db88ab
Andypengqingsong <34483498+Andypengqingsong@users.noreply.github.com>
2019-04-10 23:14:01 +0800

    新建项目
    新建项目

---
+++ taotao-manger/taotao-controller/src/main/java/com/pqszl/TestMain.java
@@ -0,0 +1,19 @@
+package com.pqszl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 彭青松
+ * @project xtone-framework
+ * @company 成都信通信息技术有限公司
+ * @date 2019/4/10 23:00
+ * @description 描述当前类的用途
+ */
+@SpringBootApplication
+public class TestMain {
+    public static void main(String[] args) {
+        SpringApplication.run(TestMain.class, args);
+    }
+
+}
---
+++ taotao-manger/taotao-controller/src/main/java/com/pqszl/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.pqszl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author 彭青松
+ * @project xtone-framework
+ * @company 成都信通信息技术有限公司
+ * @date 2019/4/10 22:59
+ * @description 描述当前类的用途
+ */
+@Controller
+public class TestController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public  String  testget(){
+       return  "index";
+    }
+}
